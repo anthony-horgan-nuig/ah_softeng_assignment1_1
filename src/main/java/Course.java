89dcb0c63c4b6221ee1f53b125aaa2203eb90d24
anthonyhorgan <ahorgan321@gmail.com>
2020-11-20 13:26:09 +0000

    addeed instance variables and getters and setters

--- src/main/java/Course.java
+++ src/main/java/Course.java
@@ -1,3 +1,67 @@
+import org.joda.time.LocalDate;
+
+import java.util.ArrayList;
+
 public class Course {
 
+    private String name;
+    private ArrayList<Module> modules;
+    private ArrayList<Student> enrolledStudents;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public Course(String name){
+        this.name = name;
+    }
+
+    public Course(String name, LocalDate startDate, LocalDate endDate){
+        this.name = name;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    //TODO mutators for modules and enrolled students
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public ArrayList<Student> getEnrolledStudents() {
+        return enrolledStudents;
+    }
+
+    public ArrayList<Module> getModules() {
+        return modules;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "name='" + name + '\'' +
+                ", modules=" + modules +
+                ", enrolledStudents=" + enrolledStudents +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                '}';
+    }
 }
